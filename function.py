4d8f3bc674636b1d24ffa876cb28fba7a6ab2bd0
shin seung jae <shinseungjae1234@gmail.com>
2022-05-17 18:33:34 +0900

    지정한 날짜까지 크롤링 완료

--- function.py
+++ function.py
@@ -1,8 +1,12 @@
+from unittest import result
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver import ActionChains
+from datetime import datetime
+import collections
+import time
 
 
 # 크롬드라이버 set 함수, 자기 컴퓨터의 chromedriver를 가져와서 쓰게함
@@ -20,11 +24,11 @@ def get_page(page_source):
     soup = BeautifulSoup(page_source, "html.parser")
     # 게시물의 한개의 단위를 post로 봄
     post = soup.find_all('article')
-    print(len(post))
 
     for i in range(len(post)):
-        date = post[i].find('time').text
-        contents = post[i].find('div', {'data-testid': 'tweetText'}).text
+        date = format_date(post[i].find('time').text)
+        contents = is_content_none(post[i].find(
+            'div', {'data-testid': 'tweetText'}))
         reply = post[i].find('div', {'data-testid': 'reply'}).text
         retweet = post[i].select_one('div[aria-label$=리트윗]').text
         like = post[i].select_one('div[aria-label$=들어요]').text
@@ -34,7 +38,46 @@ def get_page(page_source):
 
 
 # 마지막 Element로 이동하는 함수
-def go_to_final_element(driver):
+def scroll_to_last_element(driver):
     action = ActionChains(driver)
     post_list = driver.find_elements_by_tag_name('article')
     action.move_to_element(post_list[-1]).perform()
+
+
+# 리스트 안에 중복되는 요소들을 제거 해줌
+def deduplication(array):
+    result = list(map(dict, collections.OrderedDict.fromkeys(
+        tuple(sorted(d.items())) for d in array)))
+
+    return result
+
+
+# 마지막 게시물의 날짜를 추출해주는 함수
+def format_date(date):
+    if(len(date) < 5):
+        return datetime.today().strftime("%Y년 %m월 %d일")
+    elif(len(date) < 8):
+        return datetime.today().strftime("%Y년 ") + date
+    else:
+        return date
+
+
+# 지정한 날짜보다 지난 날짜는 모두 제거하는 함수
+def remove_except_selected_date(array, start_date, end_date):
+
+    while True:
+        last_date = time.strptime(array[-1].get('date'), '%Y년 %m월 %d일')
+
+        if (last_date < start_date):
+            del array[-1]
+        else:
+            break
+
+    return array
+
+
+def is_content_none(content):
+    if content is None:
+        return "None"
+    else:
+        return content.text
--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import time
 from function import *
 
-START_DATE = "2021년 1월 1일"
+# 사전 설정
+START_DATE = "2021년 11월 1일"
 URL = "https://twitter.com/BTS_twt"
-result = []
 
-# get driver and go to ULR
-driver = setChromeDriver()
-driver.implicitly_wait(10)
-driver.get(URL)
 
-# result += get_page(driver.page_source)
+def main():
+    result = []
+    start_date = time.strptime(START_DATE, "%Y년 %m월 %d일")
 
-for i in range(3):
-    time.sleep(3)
-    result += get_page(driver.page_source)
-    go_to_final_element(driver)
+    # get driver and go to ULR
+    driver = setChromeDriver()
+    driver.implicitly_wait(3)
+    driver.get(URL)
 
-for i in result:
-    print(i)
+    while True:
+        time.sleep(3)
+        result += get_page(driver.page_source)
+        scroll_to_last_element(driver)
+
+        # 설정한 날짜보다 더가면 크롤링 중단
+        last_posting_date = time.strptime(
+            result[-1].get('date'), '%Y년 %m월 %d일')
+        if(last_posting_date < start_date):
+            break
+
+    # 중복 제거
+    result = deduplication(result)
+
+    # 지정한 날짜 이 외의 날짜 모두 제거
+    result = remove_except_selected_date(result, start_date)
+
+    for i in result:
+        print(i)
+
+
+main()
